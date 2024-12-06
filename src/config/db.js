a726a4f592625bdaab3dfab33f38e8ca144da61c
sandeep0302 <gsandeepgoud0302@gmail.com>
2024-12-06 18:41:11 +0530

    backend completed

--- src/config/db.js
+++ src/config/db.js
@@ -4,7 +4,6 @@ require("dotenv").config();
 
 const connectDB = async () => {
   try {
-    console.log(process.env.MONGO_URI);
     await mongoose.connect(process.env.MONGO_URI);
     console.log("MongoDB connected");
   } catch (error) {
